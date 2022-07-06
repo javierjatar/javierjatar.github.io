9b16bc94a05679762c272e0bcb88a702a05f9bc9
javierjatar <javierjatar30@gmail.com>
2022-07-06 19:59:19 +0200

    Primero

---
+++ functions.js
@@ -0,0 +1,56 @@
+const contenedor_texto =  document.getElementById("contenedor_texto");
+const tarjeta = document.getElementById("tarjeta");
+const contact_link = document.getElementById("contact");
+const contact_section = document.getElementById("contact_section");
+var cont = 0;
+function escritura(){
+    cont++;
+    console.log(cont);
+    if(cont >= 2){
+        contact_section.style.display = "none";
+        contenedor_texto.style.display = "block";
+        //e.target.removeEventListener(e.type);
+    }else{
+    contact_section.style.display = "none";
+    contenedor_texto.style.display = "block";
+    const span = document.createElement("span");
+    //span.style.class = "typed";
+    document.getElementById('contenedor_texto').appendChild(span);
+    document.getElementById('contenedor_texto').firstChild.classList.add("typed");
+    const typed = new Typed('.typed', {
+        strings: [
+            'HOLA, SOY JAVIER :) ...',
+            'TÉCNICO SUPERIOR EN DESARROLLO DE APLICACIONES WEB...',
+            'ESTOY UN POCO OBSESIONADO DE QUE TODO ENCAJE A LA PERFECCIÓN...',
+            'QUIZÁS EN LA VIDA REAL ESTO NO ES TAN CONVENIENTE, PERO...',
+            'EN EL DISEÑO Y DESARROLLO DE SITIOS WEB...',
+            'ES UN PUNTO A FAVOR.',
+        ],
+        stringsElement: '#contenedor-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+        typeSpeed: 30, // Velocidad en mlisegundos para poner una letra,
+        startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+        backSpeed: 20, // Velocidad en milisegundos para borrrar una letra,
+        smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+        shuffle: false, // Alterar el orden en el que escribe las palabras.
+        backDelay: 1000, // Tiempo de espera despues de que termina de escribir una palabra.
+        loop: true, // Repetir el array de strings
+        loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+        showCursor: true, // Mostrar cursor palpitanto
+        cursorChar: '|', // Caracter para el cursor
+        contentType: 'html', // 'html' o 'null' para texto sin formato
+    });
+
+    }
+   
+
+
+}
+
+
+contact_link.addEventListener("click", function(e){
+    contenedor_texto.style.display = "none";
+    contact_section.style.display = "block";
+    e.target.removeEventListener(e.type, escritura);
+
+
+})
