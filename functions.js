e9a188369607556b609e605f30fd55d0c01d0fa2
javierjatar <javierjatar30@gmail.com>
2022-07-13 19:09:13 +0200

    probando

--- functions.js
+++ functions.js
@@ -3,6 +3,17 @@ const tarjeta = document.getElementById("tarjeta");
 const contact_link = document.getElementById("contact");
 const contact_section = document.getElementById("contact_section");
 var cont = 0;
+
+/*window.addEventListener("resize", function(){
+    if(screen.width <= 720){
+        contact_section.style.display = "block";
+        console.log(screen.width);
+    }else if(screen.width > 720){
+        contact_section.style.display = "flex";
+        console.log(screen.width);
+    }
+})*/
+
 function escritura(){
     cont++;
     console.log(cont);
@@ -19,12 +30,7 @@ function escritura(){
     document.getElementById('contenedor_texto').firstChild.classList.add("typed");
     const typed = new Typed('.typed', {
         strings: [
-            'HOLA, SOY JAVIER :) ...',
-            'TÉCNICO SUPERIOR EN DESARROLLO DE APLICACIONES WEB...',
-            'ESTOY UN POCO OBSESIONADO DE QUE TODO ENCAJE A LA PERFECCIÓN...',
-            'QUIZÁS EN LA VIDA REAL ESTO NO ES TAN CONVENIENTE, PERO...',
-            'EN EL DISEÑO Y DESARROLLO DE SITIOS WEB...',
-            'ES UN PUNTO A FAVOR.',
+            'HOLA, SOY JAVIER :) ...<BR> TÉCNICO SUPERIOR EN DESARROLLO DE APLICACIONES WEB. ESTOY UN POCO OBSESIONADO DE QUE TODO ENCAJE A LA PERFECCIÓN... QUIZÁ EN LA VIDA REAL ESTO NO ES TAN CONVENIENTE, PERO... EN EL DISEÑO Y DESARROLLO DE SITIOS WEB, ES UN PUNTO A FAVOR. ',
         ],
         stringsElement: '#contenedor-texto', // ID del elemento que contiene cadenas de texto a mostrar.
         typeSpeed: 30, // Velocidad en mlisegundos para poner una letra,
@@ -34,23 +40,25 @@ function escritura(){
         shuffle: false, // Alterar el orden en el que escribe las palabras.
         backDelay: 1000, // Tiempo de espera despues de que termina de escribir una palabra.
         loop: true, // Repetir el array de strings
-        loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+        loopCount: 1, // Cantidad de veces a repetir el array.  false = infinite
         showCursor: true, // Mostrar cursor palpitanto
         cursorChar: '|', // Caracter para el cursor
         contentType: 'html', // 'html' o 'null' para texto sin formato
     });
 
     }
-   
-
 
 }
 
-
 contact_link.addEventListener("click", function(e){
     contenedor_texto.style.display = "none";
-    contact_section.style.display = "block";
+    if(screen.width <= 720){
+        contact_section.style.display = "block";
+        console.log(screen.width);
+    }else if(screen.width > 720){
+        contact_section.style.display = "flex";
+        console.log(screen.width);
+    }
     e.target.removeEventListener(e.type, escritura);
-
 
 })
